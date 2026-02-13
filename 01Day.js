ad3c2308af07d5d7b7795a0775c7ac300b429bf6
Shrikant Khade <159467028+khadeshri19@users.noreply.github.com>
2026-02-13 09:36:00 +0530

    Added README

--- 01Day.js
+++ 01Day.js
@@ -1,7 +1,59 @@
-console.log("Hello world");
-
-
 /*
-Code Js --> 
+Hithesh WEB DEVELOPMENT 
+
+Why do we have javascript 
+
+to make the more stronger , interactive web pages eralier but
+Now we can make anything 
+Interactive WebPages
+Mobile Apps
+Strong Backend
+Desktop apps etc
+
+What is V8 ENgine --> 
+Googles Open source high performance Javascript and we assembly engine
+written in C++
+
+What is node js 
+Free opensource cross origin platform 
+Run time enviornment so developer can 
+create DEV appps /CLI or tools
+
+How we can execute the javascript files
+Before the code runs in run-time
+CODE FILE --> PARSING --> SYNTAX TREE
+
+                |
+                |
+                |
+
+                JIT Compiler
+
+                |
+                |
+                |
+                Byte code--> translate --> machine executable code
+
+                |
+                |
+                Code executation 
+
+
+
+PARSING --> BREAKING AND COLLECTING THE TOKENS FROM THE CODE 
+FILE
+WHEN THE PARSING HAPPENS THE TOKENIZATION PROCESS GOES ON
+
+SYNTAX TREE = syntactic structure of a program and ignores unnecessary 
+details present in a parse tree . Syntax Tree is used a
+s the final representation for generating 
+machine code or intermediate code.
+
+JIT COMPILER = JUST IN TIME COMPILER
+JS does not make all things at once just translate 
+the things into the  intermedtiatery 
 
 */
+
+console.log("Hello world");
+
